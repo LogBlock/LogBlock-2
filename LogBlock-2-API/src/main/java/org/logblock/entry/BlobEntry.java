5ea1b0b32fa8b66feedc9b0310b6aa23f2b17a2f
Ammar Askar <ammar@ammaraskar.com>
2013-08-03 07:55:08 +0500

    Added utility BlobEntry constructor and OMG UNIT TESTS!!!!11!!!!!

--- LogBlock-2-API/src/main/java/org/logblock/entry/BlobEntry.java
+++ LogBlock-2-API/src/main/java/org/logblock/entry/BlobEntry.java
@@ -6,6 +6,8 @@ import org.logblock.storage.DataStore;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 
 public abstract class BlobEntry extends AbstractEntry
@@ -13,6 +15,7 @@ public abstract class BlobEntry extends AbstractEntry
 
     @SuppressWarnings("unchecked")
     private static final Class<? extends BlobEntry>[] mappings = new Class[256];
+    private static final Map<Class<? extends BlobEntry>, Byte> classTypeMap = new HashMap<Class<? extends BlobEntry>, Byte>();
     private byte type;
 
     public BlobEntry(int id, byte type)
@@ -28,6 +31,7 @@ public abstract class BlobEntry extends AbstractEntry
     static
     {
         mappings[0] = PaintingBlob.class;
+        classTypeMap.put(PaintingBlob.class, (byte) 0);
     }
 
     public static BlobEntry create(int id, byte type)
@@ -46,6 +50,19 @@ public abstract class BlobEntry extends AbstractEntry
             }
         }
 
+        return ret;
+    }
+
+    public static <T extends BlobEntry> T create(int id, Class<T> type) throws Exception
+    {
+        T ret = null;
+        try
+        {
+            ret = type.getDeclaredConstructor(int.class, byte.class).newInstance(id, classTypeMap.get(type));
+        } catch (Exception ex)
+        {
+            DataStore.getInstance().getLogger().log(Level.SEVERE, "Could not create blob with type " + type, ex);
+        }
         return ret;
     }
 }
---
+++ LogBlock-2-API/src/test/java/org/logblock/entry/BlobTest.java
@@ -0,0 +1,32 @@
+package org.logblock.entry;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.logblock.entry.blob.PaintingBlob;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+public class BlobTest
+{
+
+    @Test
+    public void paintingTest() throws Exception
+    {
+        ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
+        DataOutputStream outStream = new DataOutputStream(byteOutput);
+
+        PaintingBlob blobOut = BlobEntry.create(1, PaintingBlob.class);
+        blobOut.setArt("artistic");
+        blobOut.setDirection((byte) 5);
+        blobOut.write(outStream);
+
+        DataInputStream inputStream = new DataInputStream(new ByteArrayInputStream(byteOutput.toByteArray()));
+        PaintingBlob blobIn = BlobEntry.create(1, PaintingBlob.class);
+        blobIn.read(inputStream);
+
+        Assert.assertEquals(blobOut, blobIn);
+    }
+}
